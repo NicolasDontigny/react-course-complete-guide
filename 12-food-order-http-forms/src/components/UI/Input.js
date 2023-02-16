73b9871c17c981380a8577cfe7e6625bac508dd8
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2023-02-16 13:50:19 +0100

    Added 12 foor order http and forms

---
+++ 12-food-order-http-forms/src/components/UI/Input.js
@@ -0,0 +1,18 @@
+// @ts-nocheck
+import React from 'react';
+
+import classes from './Input.module.css';
+
+const Input = React.forwardRef((props, ref) => (
+  <div className={classes.input}>
+    <label htmlFor={props.input.id}>{props.label}</label>
+    <input
+      ref={ref}
+      {...props.input}
+    />
+  </div>
+));
+
+Input.displayName = 'Input';
+
+export default Input;
