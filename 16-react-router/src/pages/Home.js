cc9c5e7739e3f5524e2b03680605d46e30894aa7
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2023-02-23 11:01:36 +0100

    navigating programmatically

--- 16-react-router/src/pages/Home.js
+++ 16-react-router/src/pages/Home.js
@@ -1,7 +1,22 @@
-const HomePage = () => (
-  <div>
-    <h1>My Home Page</h1>
-  </div>
-);
+import { useNavigate } from 'react-router-dom';
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate('/products');
+  };
+
+  return (
+    <>
+      <div>
+        <h1>My Home Page</h1>
+      </div>
+      <p>
+        <button onClick={navigateHandler}>Navigate to Products</button>
+      </p>
+    </>
+  );
+};
 
 export default HomePage;
