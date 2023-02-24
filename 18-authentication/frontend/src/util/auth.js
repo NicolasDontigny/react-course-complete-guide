d07b22b2f3ed7221422e29ef6328df4123e13e78
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2023-02-24 12:52:05 +0100

    Added route protection (guard)

--- 18-authentication/frontend/src/util/auth.js
+++ 18-authentication/frontend/src/util/auth.js
@@ -1,3 +1,16 @@
+import { redirect } from 'react-router-dom';
+
 export const getAuthToken = () => localStorage.getItem('token');
 
 export const tokenLoader = () => getAuthToken();
+
+export const checkAuthLoader = () => {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect('/auth?mode=login');
+  }
+
+  // Return null to avoid errors
+  return null;
+};
