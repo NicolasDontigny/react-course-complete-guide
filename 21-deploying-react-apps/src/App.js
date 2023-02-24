c778b9da44f60376dbf71056f5aa35c75c51c5f5
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2023-02-24 15:55:52 +0100

    Added build folder and firebase config

--- 21-deploying-react-apps/src/App.js
+++ 21-deploying-react-apps/src/App.js
@@ -1,9 +1,13 @@
+import { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
 import HomePage from './pages/Home';
 import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
+
+const BlogPage = lazy(() => import('./pages/Blog'));
+const BlogPostPage = lazy(() => import('./pages/Post'));
 
 const router = createBrowserRouter([
   {
@@ -17,8 +21,29 @@ const router = createBrowserRouter([
       {
         path: 'posts',
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          // { index: true, element: <BlogPage />, loader: postsLoader },
+          {
+            index: true,
+            element: (
+              // Used to wait for content to be loaded before rendering it
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>
+            ),
+            // Only import loader and blog page component when we access the page
+            loader: () => import('./pages/Blog').then((module) => module.loader()),
+          },
+          // { path: ':id', element: <PostPage />, loader: postLoader },
+          {
+            path: ':id',
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPostPage />
+              </Suspense>
+            ),
+            // Meta contains params...
+            loader: (meta) => import('./pages/Post').then((module) => module.loader(meta)),
+          },
         ],
       },
     ],
