ebb42b3d52102611f87522c7579c9a337032a354
Nicolas Dontigny <nicolas.dontigny@beslogic.com>
2023-02-27 15:21:46 +0100

    Added pages

---
+++ 22-nextjs-intro/pages/sub/[nestedId].tsx
@@ -0,0 +1,3 @@
+export default function NestedPage() {
+  return <h1>Nested Page</h1>;
+}
---
+++ 22-nextjs-intro/pages/sub/index.tsx
@@ -0,0 +1,5 @@
+// our-domain.com/sub
+
+export default function SubPage() {
+  return <h1>The Sub Page</h1>;
+}
